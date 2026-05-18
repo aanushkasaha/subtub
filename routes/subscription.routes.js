77e209961094b47949478ec6e79d18becb009a2a
aanushkasaha <sahaaanushka28@gmail.com>
2026-05-18 17:56:36 +0530

    api api

---
+++ routes/subscription.routes.js
@@ -0,0 +1,36 @@
+import { Router } from "express";
+const subscriptionRouter = Router();
+
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ message: "Fetch all subscriptions." });
+});
+
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ message: "Fetch subscription." });
+});
+
+subscriptionRouter.post("/:id", (req, res) => {
+  res.send({ message: "Create subscription." });
+});
+
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ message: "Update subscription." });
+});
+
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ message: "Delete subscription." });
+});
+
+subscriptionRouter.get("/user/:id", (req, res) => {
+  res.send({ message: "Fetch all user subscription." });
+});
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({ message: "Cancel subscription." });
+});
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({ message: "Fetch all uupcoming renewals." });
+});
+
+export default subscriptionRouter;
