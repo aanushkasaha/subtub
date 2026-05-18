e7b4062ee9e36e66db37b56d3c65f78e719dea61
aanushkasaha <sahaaanushka28@gmail.com>
2026-05-18 14:40:20 +0530

    .env

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { PORT } from "./config/env.js";
+
 const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to subtub: Your favourite Subscription Tracker API");
 });
-app.listen(3000, () => {
-  console.log("Subscription Tracker API is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on port ${PORT}`);
 });
 
 export default app;
