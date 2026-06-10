9ee87e8311bf63f00ba6929546cd5c4f7009f52d
aanushkasaha <sahaaanushka28@gmail.com>
2026-06-10 23:55:43 +0530

    ARCJETTTT

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import connect_db from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(arcjetMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
